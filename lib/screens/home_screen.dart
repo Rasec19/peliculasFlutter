a9e9004d33a81ad34a12688863b6c9ae5d6722d2
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-28 15:20:35 -0700

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Peliculas en cines'),
+          ),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.search_outlined))
+          ],
+        ),
+        body: Column(
+          children: const [CardSwiper()],
+        ));
+  }
+}
