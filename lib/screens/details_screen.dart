d356ff15441b800005ca7524effe7af847b21059
Rasec Camacho <rasec.camacho19@gmail.com>
2023-03-02 14:57:09 -0700

    details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,15 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //TODO: Cambiar luego por una instancia de movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Home Screen'),
+        body: CustomScrollView(
+      slivers: [
+        const _CustomAppBar(),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          _PosterAndTitle(),
+          _Overview(),
+          _Overview(),
+          _Overview(),
+          _Overview(),
+          _Overview(),
+          _Overview(),
+          const CastinCards()
+        ]))
+      ],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 300,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          padding: const EdgeInsets.only(bottom: 10),
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
         ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/300x500'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/loading.gif'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.yellow[800],
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Laborum nostrud sit anim pariatur sunt. Lorem qui nisi ad officia dolor sint irure. Fugiat ut eiusmod ea quis laborum commodo minim irure enim enim. Occaecat et laboris est exercitation culpa qui irure labore est ex. In consequat consequat ex laborum ex id minim nisi cupidatat quis dolore enim qui reprehenderit. Eu tempor sint elit ullamco id. Irure reprehenderit minim pariatur nisi officia voluptate est aliqua non ad pariatur irure amet velit',
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.subtitle1,
       ),
     );
   }
